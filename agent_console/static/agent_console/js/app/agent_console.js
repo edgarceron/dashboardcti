4bfbac65c5e602b654d94e93861b5040e40748e6
Edgar Mauricio Ceron Florez <edgar.ceron@correounivalle.edu.co>
2020-10-17 12:18:41 -0500

    Campaign creation

--- agent_console/static/agent_console/js/app/agent_console.js
+++ agent_console/static/agent_console/js/app/agent_console.js
@@ -1,22 +1,7 @@
 var call_consolidacion_id; 
-var data_email;
 var inTransaction = false;
 var reset = false;
 var stateChanged = false;
-
-function getValues(){
-    data = {
-        'cedula': $('#cedulaInput').val(),
-        'placa': $('#placaInput').val(),
-        'sede': $('#sedeInput').val(),
-        'fecha': $('#fechaInput').val(),
-        'hora': $('#horaInput').val(),
-        'motivo': $('#motivoInput').val(),
-        'call_consolidacion_id': $('#call_consolidacion_idInput').val(),
-    }
-    data_email = data;
-    return data;
-}
 
 function getAgentState(agent, previous_state, previous_call){ 
     console.log(inTransaction);
@@ -78,33 +63,6 @@ function getAgentState(agent, previous_state, previous_call){
 
 
 $( document ).ready(function() {
-    $('#horaInput').prop('disabled', 'disabled');
-    $('#sedeInput').selectpicker({"liveSearch": true});
-    $('#motivoInput').selectpicker({"liveSearch": true});
-    $('#generarCitaButton').click(Citas.createCita);
-    $('#gotoCitasTallerButton').click(Citas.goToCitasTaller);
-    $('#emailButton').click(Citas.sendConfirmationEmail);
-    $('#sedeInput').change(Citas.getHorariosDisponibles);
-    $('#fechaInput').change(Citas.getHorariosDisponibles);
-    $('#fechaInput').change(Citas.getHorariosDisponibles);
-    $('#noEmailButton').click(Citas.cancel);
-    $('#cancelButton').click(Citas.cancel);
-
-    urls = {
-        'get_motivo_url': {'url' : get_motivo_url, 'method':'POST'},
-        'get_sede_url': {'url' : get_sede_url, 'method':'POST'},
-        'create_cita_url': {'url' : create_cita_url, 'method':'POST'},
-        'check_horarios': {'url' : check_horarios_url, 'method':'POST'},
-        'send_confirmation_mail_url': {'url' : send_confirmation_mail_url, 'method':'POST'},
-    }
-
-    Citas.standard = new StandardCrud(urls);;
-
-    FormFunctions.setAjaxLoadPicker('#sedeInput', picker_search_sede_url, Citas.updatePickerSede, "Escoja una sede");
-    FormFunctions.setAjaxLoadPicker('#motivoInput', picker_search_motivo_url, FormFunctions.updatePicker, "Escoja un motivo");
-    FormFunctions.ajaxLoadPicker('#sedeInput', picker_search_sede_url, Citas.updatePickerSede, "", "Escoja una sede");
-    FormFunctions.ajaxLoadPicker('#motivoInput', picker_search_motivo_url, FormFunctions.updatePicker, "", "Escoja un motivo");
-
     setTimeout(function(){ 
         getAgentState(id_agent, -1, "");
     }, 1000);
