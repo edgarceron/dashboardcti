fa37e71161aa8c3798f9c604ce1de072c8eb9892
Edgar Mauricio Ceron Florez <edgar.ceron@correounivalle.edu.co>
2020-11-27 19:17:56 -0500

    Asesor multiple sede

--- asesores/models.py
+++ asesores/models.py
@@ -2,3 +2,10 @@
 from django.db import models
 from sedes.models import Sede
 
+# Create your models here.
+class Asesor(models.Model):
+    """Class for asesor model"""
+    name = models.CharField(unique=True, max_length=100, verbose_name="App")
+    active = models.BooleanField(verbose_name="Activo/Inactivo", null=False, default=True)
+    sede = models.OneToOneField(Sede, on_delete=models.CASCADE)
+    asesor_dms = models.IntegerField(null=True, default=0)
