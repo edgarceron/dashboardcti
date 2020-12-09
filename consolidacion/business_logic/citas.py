3f43ec744954e2c03ae1f5dc9f6420f4c49e7986
Edgar Mauricio Ceron Florez <edgar.ceron@correounivalle.edu.co>
2020-12-09 16:17:35 -0500

    Error citas agendadas 7:00 am no se mostraban

--- consolidacion/business_logic/citas.py
+++ consolidacion/business_logic/citas.py
@@ -235,7 +235,12 @@ def citas_en_horario(sede, fecha, hora):
             CitaNoCall.objects.filter(cita_tall_id__in=cita_ids).values_list(
                 'cita_tall_id', flat=True))
 
-        datetime_ocupados = tall_citas.filter(Q(id_cita__in=citas_call) | Q(id_cita__in=citas_no_call))
+        citas_call_entry = list(
+            CallEntryCita.objects.filter(cita_tall_id=cita_ids).values_list(
+                'cita_tall_id', flat=True))
+
+        datetime_ocupados = tall_citas.filter(
+            Q(id_cita__in=citas_call) | Q(id_cita__in=citas_no_call | Q(id_cita__in=citas_call_entry)))
 
         citas = TallCitasSerializer(datetime_ocupados, many=True)
 
