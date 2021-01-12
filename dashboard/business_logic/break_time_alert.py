af24534bcd4623ea19f2e3e0ab1099e25e25d3d1
Edgar Mauricio Ceron Florez <edgar.ceron@correounivalle.edu.co>
2021-01-12 16:19:54 -0500

    break time alert fix

--- dashboard/business_logic/break_time_alert.py
+++ dashboard/business_logic/break_time_alert.py
@@ -1,4 +1,5 @@
 import pytz
+import pendulum
 from datetime import timedelta, datetime
 from agent_console.models import Audit, Break, BreakTimes, Agent
 from agent_console.console_functions.agent_state import AgentState
@@ -7,15 +8,15 @@ from agent_console.console_functions.agent_state import AgentState
 def get_break_time_alerts():
     """Return info about the agent that have exceeded the break allowed time"""
     active_agents = Audit.objects.filter(id_break__isnull=True, datetime_end__isnull=True)
-    timezone = pytz.timezone("America/Bogota")
-    today_aware = timezone.localize(datetime.now())
+    today_aware = pendulum.instance(datetime.now(), 'America/Bogota')
     alerts = []
     for agent in active_agents:
         active_break = AgentState.get_active_break(agent.id_agent)
         if active_break != None:
             try:
                 init = active_break.datetime_init
-                total = today_aware.astimezone(pytz.utc) - init
+                init = pendulum.instance(init)
+                total = (today_aware - init).in_seconds()
                 allowed = BreakTimes.objects.get(id_break=active_break.id_break.id).minutes
                 remain = timedelta(minutes=allowed).seconds - total.seconds
                 agent = Agent.objects.get(pk=active_break.id_agent.id)
